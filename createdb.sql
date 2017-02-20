e67c1ebeeb8a0cae973ec85d79c265de49d94379
mldn <mldnqa@163.com>
2017-02-20 20:58:48 +0800

    Change Admin.java AND Emp.java in dev

---
+++ createdb.sql
@@ -0,0 +1,14 @@
+﻿public class Hello {
+	public static void main(String args[]) {
+	
+		System.out.println("世界，你好！");
+		System.out.println(new java.uti.Date());
+		Admin admin = new Admin() ;
+		Emp emp = new Emp() ;
+		SysSystem.out.println(new java.uti.Date());
+		Admin admin = new Admin() ;
+		Emp emp = new Emp() ;
+		System.out.println("我真的饿了");
+		System.out.println("多运动，");
+	}
+}
