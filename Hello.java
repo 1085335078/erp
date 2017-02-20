fe47fd668af4738858a0c354407115d6ac97a6ee
mldn <mldnqa@163.com>
2017-02-20 15:43:59 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@
 		System.out.println("认真听课的孩子才是好孩子！");
 		System.out.println("不认真听课的孩子去后面站着！");
 		System.out.println("世界，你好！");
+		System.out.println("世界，你好！");
+		System.out.println("世界，你好！");
 		System.out.println(new java.uti.Date());
 		Admin admin = new Admin() ;
 		Emp emp = new Emp() ;
