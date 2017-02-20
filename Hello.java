dec93431a9531604c6c81ecaf826108b21493ccb
mldn <mldnqa@163.com>
2017-02-20 20:20:16 +0800

    Apply patch

--- Hello.java
+++ Hello.java
@@ -5,6 +5,10 @@
 		System.out.println(new java.uti.Date());
 		Admin admin = new Admin() ;
 		Emp emp = new Emp() ;
+		SysSystem.out.println(new java.uti.Date());
+		Admin admin = new Admin() ;
+		Emp emp = new Emp() ;
 		System.out.println("我真的饿了");
+		System.out.println("多运动，");
 	}
 }
